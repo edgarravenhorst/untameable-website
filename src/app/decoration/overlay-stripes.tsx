00df187453c269200486c7e9953726cb6dcc6735
Edgar Ravenhorst <edgar@untameable.io>
2024-03-08 09:17:26 +0000

    made background texture smaller on mobile

--- src/app/decoration/overlay-stripes.tsx
+++ src/app/decoration/overlay-stripes.tsx
@@ -1,3 +1,9 @@
-export const OverlayStripes = () => {
-  return <div className='absolute inset-0 bg-stripes ' />;
-};
+import { twStyled } from '../helper/tw-styled';
+
+export const OverlayStripes = twStyled('div')([
+  'absolute',
+  'inset-0',
+  'bg-stripes',
+  'bg-[length:6px_6px]',
+  'sm:bg-[length:10px_10px]',
+]);
