553c8f37a015433116728f6fbe212052b1935285
Edgar Ravenhorst <edgar@untameable.io>
2025-03-31 07:38:01 +0000

    fix build

--- src/components/button-links.tsx
+++ src/components/button-links.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { ButtonLink } from "./button-link";
-import { useTranslation } from "react-i18next";
+// import React from "react";
+// import { ButtonLink } from "./button-link";
+// import { useTranslation } from "react-i18next";
 
-type Props = {};
-// flex flex-wrap  justify-end md:justify-center xl:flex-col gap-5 xl:justify-end  h-full xl:pb-10
+// type Props = {};
+// // flex flex-wrap  justify-end md:justify-center xl:flex-col gap-5 xl:justify-end  h-full xl:pb-10
 
-export const ButtonLinks = (props: Props) => {
-  const { t } = useTranslation();
-  return (
+// export const ButtonLinks = (props: Props) => {
+//   const { t } = useTranslation();
+//   return (
 
-  );
-};
+//   );
+// };
